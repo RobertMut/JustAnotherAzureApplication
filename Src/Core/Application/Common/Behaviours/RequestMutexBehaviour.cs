6706f3736e49f83977a1258affbdd63b0e2e4bba
Robert Mutnianski <robert.mutnianski@inetum.com>
2022-03-15 14:05:22 +0100

    Add RequestMutexBehaviour with implementation. Add RequestMutexBehaviour to DependencyInjection.cs Add RequestMutexBehaviourTests.cs

---
+++ Src/Core/Application/Common/Behaviours/RequestMutexBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+
+namespace Application.Common.Behaviours
+{
+    public class RequestMutexBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            string filename = request.GetType()
+                .GetProperty("Filename", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance)
+                .GetValue(request).ToString();
+            bool mutexExist = Mutex.TryOpenExisting(filename, out var mutex);
+
+            if (!mutexExist)
+            {
+                mutex = new Mutex(false, filename);
+            }
+
+            try
+            {
+                mutex.WaitOne();
+
+                return await next();
+            } finally
+            {
+                mutex.ReleaseMutex();
+            }
+        }
+    }
+}
